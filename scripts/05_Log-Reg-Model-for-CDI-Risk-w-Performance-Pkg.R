7462cfa418e65f2c8a57c9965fcfbf21e85a0265
Steph Reynolds <sreynolds1027@gmail.com>
2022-01-19 13:20:27 -0800

    Created scripts for fitting logistic regression models and checking assumptions

---
+++ scripts/05_Log-Reg-Model-for-CDI-Risk-w-Performance-Pkg.R
@@ -0,0 +1,82 @@
+# Logistic Regression with performance package
+
+# Install and load required packages ---- 
+install.packages("performance", dependencies = TRUE)
+library(performance)
+library(tidyverse)
+library(dlookr)
+
+# Import `df_CDI_risk_model.csv` and assign to `df` ----
+df <- read_csv("/Users/sreynolds2/Documents/GitHub/MS-CDI_Risk/MS-CDI_Risk/data/clean/df_CDI_risk_model.csv")
+
+# Create dataset with only relevant parameters (readmit, age, cdif, ppi, gas, abx) ---- 
+df <- df %>% 
+  select_if(is.numeric) %>% 
+  select(-DBM) %>% 
+  rename(cdif = cdif_gt_2_days) %>% 
+  mutate_if(is.numeric, factor)
+
+# EDA with dlookr ---- 
+dlookr::diagnose_paged_report(df)
+
+# Build logistic regression models ----
+  # m1 includes all parameters
+  m1 <- glm(data = df, formula = cdif ~ readmit + age_gte_65 + PPI + GAS + ABX, family = binomial)
+  summary(m1)
+  
+  # m2 includes all parameters except for ABX 
+  # based on summary(m1), ABX was the least significant predictor (p=0.97)
+  m2 <- glm(data = df, formula = cdif ~ readmit + age_gte_65 + PPI + GAS, family = binomial)
+
+# Compare performance between m1 and m2 ----
+compare_performance(m1, m2, rank = T)
+  
+# Compare individual quality indicators: R2, ICC, AIC, BIC ----
+  # R2
+  r2(m1)
+  r2(m2)
+  
+  # Intraclass correlation coefficient (ICC) - only used with random effects models
+  # icc(m1)
+  # icc(m2)
+  
+  # AIC
+  AIC(m1)
+  AIC(m2)
+  
+  # BIC
+  BIC(m1)
+  BIC(m2)
+
+# Check model assumptions ----
+  # m1
+  check_model(m1)
+  
+  # m2
+  check_model(m2)
+
+# Check individual model assumptions ---- 
+  # Check normality - not necessary for our log reg model, since response variable is binary
+  # check_normality(m1)
+  # check_normality(m2)
+  
+  # Check collinearity by variance inflation factor (VF)
+  check_collinearity(m1)
+  plot(check_collinearity(m1))
+  
+  # Check heteroscedasticity - not necessary for our model, since ... ??
+  # check_heteroscedasticity(m1)
+  # plot(check_heteroscedasticity(m1))
+  
+  # Check outliers
+  check_outliers(m1)  # can add method='iqr'
+  plot(check_outliers(m1))
+  
+  # Check singularity
+    # If TRUE and there is singularity, best way to deal with it is simplify the model or reduce the number of parameters.
+  check_singularity(m1)
+  check_singularity(m2)
+  
+# End of Document 
+
+  
---
+++ scripts/06_Log-Reg-Model-for-CDI-Risk-w-tidymodels-and-vip-pkgs.R
@@ -0,0 +1,88 @@
+# Logistic Regression with tidymodels and vip packages
+
+# Load required packages ---- 
+library(tidyverse)
+library(tidymodels)
+library(vip) # to visualize feature importance 
+
+# Import `df_CDI_risk_model.csv` and assign to `df` ----
+df <- read_csv("/Users/sreynolds2/Documents/GitHub/MS-CDI_Risk/MS-CDI_Risk/data/clean/df_CDI_risk_model.csv")
+
+# Create dataset with only relevant parameters (readmit, age, cdif, ppi, gas, abx) and convert to factor type ---- 
+df <- df %>% 
+  select_if(is.numeric) %>% 
+  select(-DBM) %>% 
+  rename(cdif = cdif_gt_2_days) %>% 
+  mutate_if(is.numeric, factor)
+
+# EDA with dlookr ---- 
+# dlookr::diagnose_paged_report(df)
+
+# Create train and test splits ----
+  # Set seed to ensure reproducibility 
+  set.seed(123)
+  
+  # Create initial splits 
+  splits <- df %>% initial_split(prop = 0.8)
+  
+  # View splits - can also view the training and testing splits individualy
+  splits
+    training(splits)
+    testing(splits)
+
+# Fit the model ----
+model_fit_glm <- logistic_reg() %>% 
+  set_engine("glm") %>% 
+  fit(cdif ~ . , data = training(splits))
+
+model_fit_glm
+
+# Check model assumptions ----
+check_model(model_fit_glm)
+
+# Check model performance ---- 
+model_performance(model_fit_glm)
+
+# Predict class and numeric probabilities on testing data ---- 
+  # Predict class (whether or not pt predicted to have cdiff)
+  prediction_class_test <- predict(model_fit_glm, new_data = testing(splits), type = 'class')
+  prediction_class_test
+  
+  # Predict numeric probabilities for each class (Y/N cdiff)
+  prediction_prob_test <- predict(model_fit_glm, new_data = testing(splits), type = 'prob')
+  prediction_prob_test
+  
+  # Create table that binds predicted class and probs
+    # Will be used to evaluate model in next step 
+  results_tbl <- bind_cols(prediction_class_test, prediction_prob_test, testing(splits))
+  results_tbl
+
+# Evaluate model performance: AUC and ROC ----
+  # Plot AUC 
+  results_tbl %>% roc_auc(cdif, .pred_0)
+  # AUC = 0.845
+  
+  # Visualize ROC 
+  results_tbl %>% roc_curve(cdif, .pred_0) %>% 
+    autoplot()
+
+# Feature importance ----
+  # Visualize most important features 
+  model_fit_glm %>% 
+    vip(num_features = 5,
+        geom = "point",
+        aes = list(size = 3, color = '18bc9c')) + 
+    theme_minimal(base_size = 16) + 
+    labs(title = "Logistic Regression: Feature Importance ")
+    # Features with highest importance are: PPI and GAS 
+
+  # Visualize top features
+  df %>% ggplot(aes(cdif, PPI, color = GAS)) +
+    geom_jitter(alpha = 0.25) + 
+    theme_minimal(base_size = 16) +
+    scale_color_viridis_d(end = 0.4) + 
+    labs(title = "Comparison of Predicted CDIF Diagnosis\nusing PPI and GAS as Key Predictors")
+
+  
+# End of Document
+
